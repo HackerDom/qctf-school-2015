d79d1b78f93501dc9055453495cfce3502f5d900
Tinkturianec <zaynullin-bulat@yandex.ru>
2015-05-08 01:30:12 +0500

    add vim task

---
+++ tasks/vim/gen.cpp
@@ -0,0 +1,20 @@
+#include <cstdio>
+
+unsigned seed = 0x2123;
+
+unsigned rand() {
+	seed = (seed * 0x93af7 ^ 0x7da95b88) + 0x156a8e5f;
+	return seed;
+}
+
+int main() {
+	freopen("flag.txt", "w", stdout);
+	for (int i = 0; i < 500 * 1024 * 1024; ++i) {
+		int r = rand() % (126 - 32 + 1 + 1);
+		if (!r)
+			putchar('\n');
+		else
+			putchar(31 + r);
+	}
+	return 0;
+}
