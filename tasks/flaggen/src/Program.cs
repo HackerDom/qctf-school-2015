4313e1a6cf60b6d23a0a1c8255406cdd99d6bf66
Dmitriy Titarenko <mail.dscheg@gmail.com>
2015-05-09 03:01:08 +0300

    flaggen task

---
+++ tasks/flaggen/src/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace flaggen
+{
+	internal static class Program
+	{
+		private static void Main()
+		{
+			var sb = new StringBuilder("QCTF");
+			Enumerable.Range(20, 4).Select(Fib).ForEach(i => sb.Append(i.ToString("X")));
+			Console.WriteLine(sb);
+		}
+
+		private static int Fib(int n)
+		{
+			if(n <= 1) return n;
+			return Fib(n - 1) + Fib(n - 2);
+		}
+	}
+
+	internal static class EnumerableHelper
+	{
+		public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+		{
+			foreach(var item in enumerable)
+				action(item);
+		}
+	}
+}
